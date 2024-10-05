5e91f8886d5260a4112fa8633240c3b396496353
Michal Jenikovsky <jendo@jmsystems.sk>
2024-10-05 05:03:04 +0200

    fetch.sh: fetch only first commit shallow
     - this makes faster fetching the same refs multiple times
     - also allows to checkout over local changes in repository
    
    Signed-off-by: Michal Jenikovsky <jendo@jmsystems.sk>

--- fetch.sh
+++ fetch.sh
@@ -5,10 +5,12 @@ REPO_URL="$2"
 FETCH_ALL="$3"
 REPO_FOLDER=$(basename "$REPO_URL")
 REPO_FOLDER=build/${REPO_FOLDER%.*}
+DEPTH=""
 echo "Fetching '$REPO_FOLDER' version '$REPO_REF'..."
 if [ ! -d "$REPO_FOLDER" ]; then
     git init "$REPO_FOLDER"
     git -C "$REPO_FOLDER" remote add origin "$REPO_URL"
+    DEPTH="--depth=1"
 else
     git -C "$REPO_FOLDER" remote set-url origin "$REPO_URL"
 fi
@@ -16,6 +18,6 @@ if [[ "$FETCH_ALL" == true ]]; then
     git -C "$REPO_FOLDER" fetch --unshallow || true
     git -C "$REPO_FOLDER" fetch origin "$REPO_REF" --tags
 else
-    git -C "$REPO_FOLDER" fetch origin "$REPO_REF" --depth=1
+    git -C "$REPO_FOLDER" fetch origin "$REPO_REF" $DEPTH
 fi
-git -C "$REPO_FOLDER" checkout -f FETCH_HEAD
+git -C "$REPO_FOLDER" checkout FETCH_HEAD
